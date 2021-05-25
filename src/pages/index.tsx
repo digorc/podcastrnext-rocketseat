1a7be02c7512280b09ae0cb7a2ec970889a528ce
digorc <rodrigo_123correia321@hotmail.com>
2021-05-25 07:44:53 -0300

    spa ssr ssg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,46 @@
-export default function Home() {
+// SPA
+// SSR
+// SSG
+
+import { useEffect } from "react"
+
+
+export default function Home(props) {
+  // useEffect(() => { // SPA
+  //   fetch('http://localhost:3333/episodes')
+  //     .then(response => response.json())
+  //     .then(data => console.log(data))
+  // }, [])
+
+  // console.log(props.episodes)
+
   return (
-    <h1>Hello World!</h1>
+    <>
+      <p>Index</p>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </>
   )
+}
+
+// export async function getServerSideProps() { // SSR
+//   const response = await fetch('http://localhost:3333/episodes')
+//   const data = await response.json()
+
+//   return {
+//     props: {
+//       episodes: data,
+//     }
+//   }
+// }
+
+export async function getStaticProps() { // SSG
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60*60*8
+  }
 }
